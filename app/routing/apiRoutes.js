e8388e1417d21de00b1807d51d1ad6575a910d91
Kelsey Tobin <etobinkelsey@gmail.com>
2019-02-28 13:05:13 -0700

    post function working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,20 @@
+var path = require("path");
+
+//pull in data we need for api routes
 var friendData = require("../data/friends.js");
 
 module.exports = function (app) { //app to represent express
 
-    app.get("/api/friends", function (req, res) {
-        res.json(friendData);
+    app.get("/api/list", function (req, res) {
+        return res.json(friendData);
     });
 
-    app.post("/api/friends", function (req, res) {
+    app.post("/api/list", function (req, res) {
         var newFriend = req.body;
+
+        friendData.push(newFriend);
+        res.json(true);
         
-        console.log(newFriend);
 
     });
 
--- server.js
+++ server.js
@@ -28,19 +28,18 @@ app.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname, "/app/public/survey.html"));
 });
 
-
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/app/public/home.html"));
 });
-app.get("/", function (req, res) {
-    res.json(path.join(__dirname, "/app/public/home.html"));
-});
+// app.get("/", function (req, res) {
+//     res.json(path.join(__dirname, "/app/public/home.html"));
+// });
 
 // ==============================================================
 
 //require routing files, api file first to then display within html
-// require("./app/routing/apiRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 
