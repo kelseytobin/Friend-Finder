d2ac143ab8f44b17623f19bb34ebdaf1facc746c
Kelsey Tobin <etobinkelsey@gmail.com>
2019-02-28 13:41:33 -0700

    working match function and modal display

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,9 +11,29 @@ module.exports = function (app) { //app to represent express
 
     app.post("/api/list", function (req, res) {
         var newFriend = req.body;
+        var newScore = newFriend.scores;
+        var scoreTotal = 0;
+        var highestMatch = 100;
+        var index = -1;
+
+        //loop through every entry in friend away, calculate and compare score total to retrieve match
+        for (var i = 0; i < friendData.length; i++) {
+            
+            total = 0;
+
+            for (var j = 0; j < newScore.length; j++) {
+                
+                var diff = Math.abs(newScore[j] - friendData[i].scores[j]);
+                scoreTotal += diff;
+            }
+            if (scoreTotal < highestMatch) {
+                highestMatch = scoreTotal;
+                index = i;
+            }
+        }
 
         friendData.push(newFriend);
-        res.json(true);
+        res.json(friendData[index]);
         
 
     });
